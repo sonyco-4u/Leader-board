59e3ab9dd94842914fd19cae8cce018b931c5519
Ezema Anthony Sunday <tonyunday@gmail.com>
2022-10-19 14:14:53 -0700

    updated Readme

--- src/index.js
+++ src/index.js
@@ -1,9 +1,1 @@
 import './style.css';
-// import { displayScores } from './listscore';
-
-// displayScores();
-// const tittle = document.createElement('h3');
-// tittle.textContent = 'Webpack made easy!';
-
-// const page = document.querySelector('body');
-// page.append(title);

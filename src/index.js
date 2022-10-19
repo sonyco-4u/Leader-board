e6c7158bdca59a3147c894da6a28d79941e869a6
Ezema Anthony Sunday <tonyunday@gmail.com>
2022-10-19 14:06:50 -0700

    updated Readme

--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
 import './style.css';
-const tittle = document.createElement("h3");
-tittle.textContent = "Webpack made easy!";
+const tittle = document.createElement('h3');
+tittle.textContent = 'Webpack made easy!';
 
-const page = document.querySelector("body");
+const page = document.querySelector('body');
 page.append(title);
